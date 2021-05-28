2ec71a7f1a7c9c1e3ee98343d09cd357d849cb7d
Nicola Piga <nicolapiga@gmail.com>
2021-05-28 14:27:39 +0200

    Add copyright in testing scripts

--- test/nrt/test_icp.sh
+++ test/nrt/test_icp.sh
@@ -1,3 +1,12 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
 OBJ_NAMES=`ls ./test/objects`
 
 for obj_name in $OBJ_NAMES;
