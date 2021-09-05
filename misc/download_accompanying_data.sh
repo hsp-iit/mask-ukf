b0bc629ea8e9f99f750d7ed46351496b05e7f00f
Nicola Piga <nicolapiga@gmail.com>
2021-09-05 10:52:48 +0200

    [misc] Add script for downloading accompaying data

---
+++ misc/download_accompanying_data.sh
@@ -0,0 +1,11 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+wget https://zenodo.org/record/5419201/files/data.zip?download=1 -O data.zip
+unzip data.zip
